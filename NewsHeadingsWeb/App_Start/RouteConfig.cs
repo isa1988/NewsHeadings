902824553627c23d515ecd65a95b4c50a2351def
Isa <isa.ibragimov1988@gmail.com>
2019-04-11 12:56:17 +0600

    поправил размтку

--- NewsHeadingsWeb/App_Start/RouteConfig.cs
+++ NewsHeadingsWeb/App_Start/RouteConfig.cs
@@ -12,7 +12,13 @@ namespace NewsHeadingsWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
+            routes.MapRoute(
+                name: "NewsControllers",
+                url: "News/Show/{pathLink}",
+                defaults: new { controller = "News", action = "Show" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
